5c4536feca93bfe75ebfba148daf255cfdae39ab
narendiran <narendiran@twilightsoftwares.com>
2022-01-22 19:42:09 +0530

    feat: router line added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 const userController = require('../controllers/user.controller')
+const express = require('express') 
 
-const router = require('express').Router()
+const router = express.Router();
 
 
 router.get('/getName', userController.getName);
